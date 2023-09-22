a910eca925f860c8d2d9d8d347efafb39d51f2f3
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-09-22 21:51:30 +0200

    styled inner card html elements

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -12,13 +12,22 @@ const Card = (props: CardProps) => {
   return (
     <div className={classes.card}>
       <header>
-        <img src={props.image} alt="country flag" />
+        <img className={classes.image} src={props.image} alt="country flag" />
       </header>
       <div>
-        <h1>{props.name}</h1>
-        <p>{props.population}</p>
-        <p>{props.region}</p>
-        <p>{props.capital}</p>
+        <h1 className={classes.name}>{props.name}</h1>
+        <div className={classes["population-wrapper"]}>
+          <p className={classes["population__text"]}>Population:</p>
+          <p className={classes["population__number"]}>{props.population}</p>
+        </div>
+        <div className={classes["region-wrapper"]}>
+          <p className={classes["region__text"]}>Region:</p>
+          <p className={classes["region__number"]}>{props.region}</p>
+        </div>
+        <div className={classes["capital-wrapper"]}>
+          <p className={classes["capital__text"]}>Capital:</p>
+          <p className={classes["capital__number"]}>{props.capital}</p>
+        </div>
       </div>
     </div>
   );
