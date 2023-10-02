7f363aee091a20b5bae11139c91440996ac92239
Scerbak <ts3820@intl.att.com>
2023-10-02 15:21:08 +0200

    changed img for svg and added theme colors to fonts

--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -1,13 +1,11 @@
 import { useState, useContext } from "react";
 
-import Arrow from "../../assets/angle-down-solid.svg";
-
 import classes from "./Dropdown.module.css";
 
 import ThemeContext from "../../store/theme-context";
 
 const Dropdown = ({ handleContinent, continent }: any) => {
-  const { setThemeStyles } = useContext(ThemeContext);
+  const { setThemeStyles, theme } = useContext(ThemeContext);
   const [open, isOpen] = useState<boolean>(false);
 
   const continets = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
@@ -21,9 +19,19 @@ const Dropdown = ({ handleContinent, continent }: any) => {
       <div
         className={`${classes.filter} ${setThemeStyles("backgroundHeader")}`}
       >
-        <span>{continent ? continent : "Filter by Region"}</span>
+        <span className={`${setThemeStyles("fontColor")}`}>
+          {continent ? continent : "Filter by Region"}
+        </span>
         <span>
-          <img className={classes.arrow} src={Arrow} alt="dropdown arrow" />
+          <svg
+            fill={theme === "light" ? "#111517" : "#ffff"}
+            className={classes.arrow}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+          </svg>
+          {/* <img className={classes.arrow} src={Arrow} alt="dropdown arrow" /> */}
         </span>
       </div>
       {open ? (
@@ -34,7 +42,11 @@ const Dropdown = ({ handleContinent, continent }: any) => {
         >
           <ul>
             {continets.map((continent, id) => (
-              <li onClick={handleContinent} key={id}>
+              <li
+                className={setThemeStyles("fontColor")}
+                onClick={handleContinent}
+                key={id}
+              >
                 {continent}
               </li>
             ))}
