58bfbdbb706e0e4c77f61d08a7ec8f82466b9b22
Scerbak <ts3820@intl.att.com>
2023-10-02 15:26:26 +0200

    added theme styles

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -20,18 +20,52 @@ const Card = (props: CardProps) => {
         <img className={classes.image} src={props.image} alt="country flag" />
       </header>
       <div className={classes["card-body"]}>
-        <h1 className={classes.name}>{props.name}</h1>
+        <h1 className={`${classes.name} ${setThemeStyles("fontColor")}`}>
+          {props.name}
+        </h1>
         <div className={classes["population-wrapper"]}>
-          <p className={classes["population__text"]}>Population:</p>
-          <p className={classes["population__number"]}>{props.population}</p>
+          <p
+            className={`${classes["population__text"]} ${setThemeStyles(
+              "fontColor"
+            )}`}
+          >
+            Population:
+          </p>
+          <p className={`${classes["number"]} ${setThemeStyles("fontColor")}`}>
+            {props.population}
+          </p>
         </div>
         <div className={classes["region-wrapper"]}>
-          <p className={classes["region__text"]}>Region:</p>
-          <p className={classes["region__number"]}>{props.region}</p>
+          <p
+            className={`${classes["region__text"]} ${setThemeStyles(
+              "fontColor"
+            )}`}
+          >
+            Region:
+          </p>
+          <p
+            className={`${classes["region__number"]} ${setThemeStyles(
+              "fontColor"
+            )}`}
+          >
+            {props.region}
+          </p>
         </div>
         <div className={classes["capital-wrapper"]}>
-          <p className={classes["capital__text"]}>Capital:</p>
-          <p className={classes["capital__number"]}>{props.capital}</p>
+          <p
+            className={`${classes["capital__text"]} ${setThemeStyles(
+              "fontColor"
+            )}`}
+          >
+            Capital:
+          </p>
+          <p
+            className={`${classes["capital__number"]} ${setThemeStyles(
+              "fontColor"
+            )}`}
+          >
+            {props.capital}
+          </p>
         </div>
       </div>
     </div>
